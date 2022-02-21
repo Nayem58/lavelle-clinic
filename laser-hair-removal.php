d8620d5d6d73a7a845163df35de6a8f9a8cf6146
Shadow <Nayem58@users.noreply.github.com>
2022-02-21 16:02:43 +0600

    style.css update: why-choose-list's css is moved to global check-square's css;

--- laser-hair-removal.php
+++ laser-hair-removal.php
@@ -29,8 +29,18 @@ require_once 'inc/head.php';
   <div class="common-sec tile-bg">
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-md-6"></div>
-        <div class="col-md-6"></div>
+        <div class="col-md-6 mb-3 mb-md-0">
+          <div class="img-box vdo-box">
+            <img class=" lazyloaded" src="images/gallery/g-thumb-1.jpg" data-src="images/gallery/g-thumb-1.jpg" alt="">
+            <a class="play-btn" data-fancybox="" href="videos/vdo-1.mp4"></a>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h1 class="common-heading">
+          ALMA
+          <span class="site-primary-color-text">Soprano Titanium</span>
+        </h1>
+        </div>
       </div>
     </div>
   </div>

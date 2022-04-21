c673d1bc7ea271f85ad85c5f0f26a5dd52c716ae
Shadow <Nayem58@users.noreply.github.com>
2022-04-22 04:20:52 +0600

    added 10 new videos replacing the old ones to the video gallery row on the laser-hair-removal page; created a laser-hair-removal-2.php file

---
+++ laser-hair-removal-2.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+  $title = "Laser Hair Treatments | Lavelle Clinic";
+  require_once 'inc/head.php';
+  ?>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js" integrity="sha512-DUC8yqWf7ez3JD1jszxCWSVB0DMP78eOyBpMa5aJki1bIRARykviOuImIczkxlj1KhVSyS16w2FSQetkD4UU2w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+
+<body>
+  <!-- preloader -->
+  <div class="preLoader">
+    Loading<span>...</span>
+  </div>
+
+  <!-- header -->
+  <?php include_once 'layouts/Header.php'; ?>
+
+  <!-- main content -->
+  <main>
+    <!-- hero-banner-sec -->
+    <div class="common-sec common-sec--hero-banner common-sec--laser-hair-removal-banner py-0 flex-hv-center">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 order-lg-last">
+            <div class="img-box img-box--soprano-machine">
+              <img src="images/soprano-titanium-machine.png" alt="Soprano Titanium Machine" />
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="common-heading">
+              <div class="img-box"><img src="images/soprano-titanium-logo.png" alt="Soprano Titanium Logo"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="common-sec tile-bg">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 mb-3 mb-md-0 order-md-last">
+            <div class="vdo-box">
+              <video width="320" height="240" controls>
+                <source src="videos/alma-plus-subs.mp4" type="video/mp4">
+                <source src="videos/alma-plus-subs-og.png" type="video/ogg">
+              </video>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <h1 class="common-heading">
+              ALMA
+              <span class="site-primary-color-text">Soprano Titanium</span>
+            </h1>
+            <ul class="check-square mb-0">
+              <li>4cm2 hand piece for faster treatments and better patient experience</li>
+              <li>Ice Plus contact cooling and in motion technique for a pain free treatment</li>
+              <li>3 simultaneous wavelengths in 1 applicator</li>
+              <li>Suitable for all skin types including dark and tanned skin</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="common-sec py-0">
+      <div class="laser-hair-removal-offers-sliders owl-carousel owl-theme">
+        <div class="item">
+          <div class="img-box">
+            <img src="images/laser-hair-removal-offer-01.jpg" alt="Laser Hair Removal Offer Banner">
+          </div>
+        </div>
+        <div class="item">
+          <div class="img-box">
+            <img src="images/laser-hair-removal-offer-02.jpg" alt="Laser Hair Removal Offer Banner">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="common-sec tile-bg">
+      <div class="container">
+        <h2 class="common-heading text-center mb-50px">
+          Sundes
+          <span class="site-primary-color-text">Videos</span>
+        </h2>
+        <div class="laser-hair-removal-vdo-gallery">
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-1.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-2.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-6.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-9.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-3.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-4.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-5.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-7.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-8.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+
+          <div class="vdo-box">
+            <video width="320" height="240" controls>
+              <source src="videos/vdo-10.mp4" type="video/mp4">
+              <!-- <source src="videos/alma-plus-subs-og.png" type="video/ogg"> -->
+            </video>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php
+    include_once 'layouts/ReviewSec.php';
+    ?>
+  </main>
+
+  <!-- footer -->
+  <?php include_once 'layouts/Footer.php'; ?>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+</html>
